afe7ea9233578aebd59f8f2e0af1c0e50cb18fdd
giftchule12 <giftchule@gmail.com>
2022-02-07 11:27:38 +0200

    update form processing
    - added javascript alert and redirect

--- process/form.php
+++ process/form.php
@@ -1,4 +1,4 @@
-\<?php
+<?php
     require '../includes/connection.php';
     require '../includes/sanitize.php';                                                                                          
 
@@ -11,40 +11,46 @@
         $row = json_decode($data, true);
         
         if ($row['success'] == "true"){
-          echo "Valid";
-        }
-        else{
-          echo "Invalid";
-        }
+          $fname = sanitizeString($_POST['fname']);
+          $lname = sanitizeString($_POST['lname']);
+          $dob = sanitizeString($_POST['dob']);
+          $gender = sanitizeString($_POST['gender']);
+          $email = sanitizeString($_POST['email']);
+          $postal = sanitizeString($_POST['postal']);
+          $physical = sanitizeString($_POST['postal']);
+          $district = sanitizeString($_POST['physical']);
+          $main = sanitizeString($_POST['main']);
+          $alternate = sanitizeString($_POST['alternate']);
+          $emg = sanitizeString($_POST['emg']);
+          $campus = sanitizeString($_POST['campus']);
+          $prog = sanitizeString($_POST['prog']);
+          $intake = sanitizeInt($_POST['intake']);
+
+          $query = "INSERT INTO students (fname, lname, dob, gender, email, postal_add, physical_add, district, main_number, alt_number, emg_number, campus, prog, intake)
+                    VALUES ('$fname', '$lname', '$dob', '$gender', '$email', '$postal' , '$physical', '$district', '$main', '$alternate', '$emg', '$campus', '$prog', '$intake')";
+
+          if ($conn->query($query) === TRUE) {
+              echo '<script>';
+              echo 'alert("Student enrolled successfully");';
+              echo 'document.location.href = "../index.html";';
+              echo '</script>';
+            } else {
+              echo '<script>';
+              echo 'alert("Something is wrong please try again");';
+              echo 'document.location.href = "../index.html";';
+              echo '</script>';
+            }
+            
+            $conn->close();
+          }
+
+          else{
+            echo '<script>';
+            echo 'alert("Please confirm that you are not a robot");';
+            echo 'document.location.href = "../index.html";';
+            echo '</script>';
+          }
       }
 
-    /* $fname = sanitizeString($_POST['fname']);
-    $lname = sanitizeString($_POST['lname']);
-    $dob = $_POST['dob'];
-    $gender = $_POST['gender'];
-    $email = $_POST['email'];
-    $postal = $_POST['postal'];
-    $physical = $_POST['postal'];
-    $district = $_POST['physical'];
-    $main = $_POST['main'];
-    $alternate = $_POST['alternate'];
-    $emg = $_POST['emg'];
-    $campus = $_POST['campus'];
-    $prog = $_POST['prog'];
-    $intake = $_POST['intake'];
     
-    print $fname;
-    print "</br>";
-    print $lname;
-
-    $query = "INSERT INTO students (fname, lname, dob, gender, email, postal_add, physical_add, district, main_number, alt_number, emg_number, campus, prog, intake)
-              VALUES ('$fname', '$lname', '$dob', '$gender', '$email', '$postal' , '$physical', '$district', '$main', '$alternate', '$emg', '$campus', '$prog', '$intake')";
-
-    if ($conn->query($query) === TRUE) {
-        echo '<script>alert("Student enrolled successfully.")</script>';
-      } else {
-        echo "Error: " . $qu0ery . "<br>" . $conn->error;
-      }
-      
-      $conn->close(); */
 ?>
